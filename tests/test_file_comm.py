49685da26a6a2f7ad13e7d9bbe94cde52cacabe6
philbucher <philipp.bucher@tum.de>
2019-12-09 18:34:30 +0100

    cleanup in tests

--- tests/test_file_comm.py
+++ tests/test_file_comm.py
@@ -9,10 +9,10 @@ class TestFileCommUtilities(unittest.TestCase):
     file_in_dir_name = os.path.join(dir_name,file_name)
 
     def setUp(self):
-        self.remove_leftovers
+        self.remove_leftovers()
 
     def tearDown(self):
-        self.remove_leftovers
+        self.remove_leftovers()
 
     def remove_leftovers(self):
         if os.path.isdir(TestFileCommUtilities.dir_name):
@@ -21,18 +21,12 @@ class TestFileCommUtilities(unittest.TestCase):
             os.remove(TestFileCommUtilities.file_name)
 
     def test_create_directory(self):
-        if os.path.isdir(TestFileCommUtilities.dir_name):
-            shutil.rmtree(TestFileCommUtilities.dir_name)
-
         self.assertFalse(os.path.isdir(TestFileCommUtilities.dir_name))
         communication_tests.FileCommUtils.create_directory(TestFileCommUtilities.dir_name)
         self.assertTrue(os.path.isdir(TestFileCommUtilities.dir_name))
         shutil.rmtree(TestFileCommUtilities.dir_name)
 
     def test_remove_file(self):
-        if os.path.isfile(TestFileCommUtilities.file_name):
-            os.remove(TestFileCommUtilities.file_name)
-
         with open(TestFileCommUtilities.file_name, 'w') as test_file:
             test_file.write("hello")
 
@@ -41,8 +35,6 @@ class TestFileCommUtilities(unittest.TestCase):
         self.assertFalse(os.path.isdir(TestFileCommUtilities.file_name))
 
     def test_remove_empty_dir(self):
-        if os.path.isdir(TestFileCommUtilities.dir_name):
-            shutil.rmtree(TestFileCommUtilities.dir_name)
         os.mkdir(TestFileCommUtilities.dir_name)
 
         self.assertTrue(os.path.isdir(TestFileCommUtilities.dir_name))
@@ -50,8 +42,6 @@ class TestFileCommUtilities(unittest.TestCase):
         self.assertFalse(os.path.isdir(TestFileCommUtilities.dir_name))
 
     def test_remove_full_dir(self):
-        if os.path.isdir(TestFileCommUtilities.dir_name):
-            shutil.rmtree(TestFileCommUtilities.dir_name)
         os.mkdir(TestFileCommUtilities.dir_name)
 
         with open(TestFileCommUtilities.file_in_dir_name, 'w') as test_file:
