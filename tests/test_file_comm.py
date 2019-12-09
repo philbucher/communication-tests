7eea7051d6df1ef410804972639390ac378016e9
philbucher <philipp.bucher@tum.de>
2019-12-09 15:54:57 +0100

    improved test

--- tests/test_file_comm.py
+++ tests/test_file_comm.py
@@ -4,56 +4,66 @@ import sys, os, shutil
 import communication_tests
 
 class TestFileCommUtilities(unittest.TestCase):
+    dir_name = "my_dir"
+    file_name = "the_file.dat"
+    file_in_dir_name = os.path.join(dir_name,file_name)
+
+    def setUp(self):
+        self.remove_leftovers
+
+    def tearDown(self):
+        self.remove_leftovers
+
+    def remove_leftovers(self):
+        if os.path.isdir(TestFileCommUtilities.dir_name):
+            shutil.rmtree(TestFileCommUtilities.dir_name)
+        if os.path.isfile(TestFileCommUtilities.file_name):
+            os.remove(TestFileCommUtilities.file_name)
 
     def test_create_directory(self):
-        dir_name = "abc"
-        if os.path.isdir(dir_name):
-            shutil.rmtree(dir_name)
+        if os.path.isdir(TestFileCommUtilities.dir_name):
+            shutil.rmtree(TestFileCommUtilities.dir_name)
 
-        self.assertFalse(os.path.isdir(dir_name))
-        communication_tests.FileCommUtils.create_directory(dir_name)
-        self.assertTrue(os.path.isdir(dir_name))
-        shutil.rmtree(dir_name)
+        self.assertFalse(os.path.isdir(TestFileCommUtilities.dir_name))
+        communication_tests.FileCommUtils.create_directory(TestFileCommUtilities.dir_name)
+        self.assertTrue(os.path.isdir(TestFileCommUtilities.dir_name))
+        shutil.rmtree(TestFileCommUtilities.dir_name)
 
     def test_remove_file(self):
-        file_name = "fd.kg"
-        if os.path.isfile(file_name):
-            os.remove(file_name)
+        if os.path.isfile(TestFileCommUtilities.file_name):
+            os.remove(TestFileCommUtilities.file_name)
 
-        with open(file_name, 'w') as test_file:
+        with open(TestFileCommUtilities.file_name, 'w') as test_file:
             test_file.write("hello")
 
-        self.assertTrue(os.path.isfile(file_name))
-        communication_tests.FileCommUtils.remove(file_name)
-        self.assertFalse(os.path.isdir(file_name))
+        self.assertTrue(os.path.isfile(TestFileCommUtilities.file_name))
+        communication_tests.FileCommUtils.remove(TestFileCommUtilities.file_name)
+        self.assertFalse(os.path.isdir(TestFileCommUtilities.file_name))
 
     def test_remove_empty_dir(self):
-        dir_name = "fdg"
-        if os.path.isdir(dir_name):
-            shutil.rmtree(dir_name)
-        os.mkdir(dir_name)
+        if os.path.isdir(TestFileCommUtilities.dir_name):
+            shutil.rmtree(TestFileCommUtilities.dir_name)
+        os.mkdir(TestFileCommUtilities.dir_name)
 
-        self.assertTrue(os.path.isdir(dir_name))
-        communication_tests.FileCommUtils.remove(dir_name)
-        self.assertFalse(os.path.isdir(dir_name))
+        self.assertTrue(os.path.isdir(TestFileCommUtilities.dir_name))
+        communication_tests.FileCommUtils.remove(TestFileCommUtilities.dir_name)
+        self.assertFalse(os.path.isdir(TestFileCommUtilities.dir_name))
 
     def test_remove_full_dir(self):
-        dir_name = "tzui"
-        file_name = os.path.join(dir_name, "test_data.dat")
-        if os.path.isdir(dir_name):
-            shutil.rmtree(dir_name)
-        os.mkdir(dir_name)
+        if os.path.isdir(TestFileCommUtilities.dir_name):
+            shutil.rmtree(TestFileCommUtilities.dir_name)
+        os.mkdir(TestFileCommUtilities.dir_name)
 
-        with open(file_name, 'w') as test_file:
+        with open(TestFileCommUtilities.file_in_dir_name, 'w') as test_file:
             test_file.write("hello")
 
-        self.assertTrue(os.path.isdir(dir_name))
-        self.assertTrue(os.path.isfile(file_name))
+        self.assertTrue(os.path.isdir(TestFileCommUtilities.dir_name))
+        self.assertTrue(os.path.isfile(TestFileCommUtilities.file_in_dir_name))
         with self.assertRaisesRegex(RuntimeError, "not empty"):
-            communication_tests.FileCommUtils.remove(dir_name)
+            communication_tests.FileCommUtils.remove(TestFileCommUtilities.dir_name)
 
-        communication_tests.FileCommUtils.remove_all(dir_name)
-        self.assertFalse(os.path.isdir(dir_name))
+        communication_tests.FileCommUtils.remove_all(TestFileCommUtilities.dir_name)
+        self.assertFalse(os.path.isdir(TestFileCommUtilities.dir_name))
 
     def test_FileExists(self):
         pass
