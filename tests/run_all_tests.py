e0a5bc262db2987443e14c471f05cd06f3a04b5c
philbucher <philipp.bucher@tum.de>
2019-12-09 15:43:40 +0100

    inverting exit code

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -4,4 +4,4 @@ import os, sys
 loader = unittest.TestLoader()
 tests = loader.discover(os.path.dirname(__file__)) # automatically discover all tests in this directory
 testRunner = unittest.runner.TextTestRunner(verbosity=1)
-sys.exit(testRunner.run(tests).wasSuccessful())
+sys.exit(not testRunner.run(tests).wasSuccessful())
