7ac7dca9175268dc9a2506692e2291c8759cbc55
philbucher <philipp.bucher@tum.de>
2019-12-09 15:34:18 +0100

    try test return

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -4,4 +4,4 @@ import os
 loader = unittest.TestLoader()
 tests = loader.discover(os.path.dirname(__file__)) # automatically discover all tests in this directory
 testRunner = unittest.runner.TextTestRunner(verbosity=1)
-testRunner.run(tests)
+return testRunner.run(tests)
