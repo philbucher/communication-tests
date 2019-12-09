b6e51c12799f24e4fb34f809e42d44287f118706
philbucher <philipp.bucher@tum.de>
2019-12-09 19:03:14 +0100

    simplifications

--- file_comm/add_file_comm_to_python.h
+++ file_comm/add_file_comm_to_python.h
@@ -2,16 +2,55 @@
 
 #include <pybind11/pybind11.h>
 
-#include "file_comm_utils.h"
+#include <string>
+#include <iostream>
+
+#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include) && __has_include(<filesystem>)
+    #include <filesystem>
+    namespace fs = std::filesystem;
+    #define FILESYSTEM_AVAILABLE
+#else
+    #include "../external_libraries/ghc/filesystem.hpp"
+    namespace fs = ghc::filesystem;
+#endif
+
+namespace Helpers {
+
+void create_directory(const std::string& rDirectoryName)
+{
+    fs::create_directory(rDirectoryName);
+}
+
+void remove(const std::string& rName)
+{
+    fs::remove(rName);
+}
+
+void remove_all(const std::string& rName)
+{
+    fs::remove_all(rName);
+}
+
+bool exists(const std::string& rName)
+{
+    return fs::exists(rName);
+}
+
+void rename(const std::string& rOldName, const std::string& rNewName)
+{
+    fs::rename(rOldName, rNewName);
+}
+
+} // helpers namespace
 
 void AddFileCommToPython(pybind11::module& m)
 {
-    auto m_file_comm_utils = m.def_submodule("FileCommUtils");
+    auto m_file_comm_utils = m.def_submodule("FileSystem");
 
-    m_file_comm_utils.def("create_directory", &FileCommUtils::create_directory);
-    m_file_comm_utils.def("rename",           &FileCommUtils::rename);
-    m_file_comm_utils.def("remove",           &FileCommUtils::remove);
-    m_file_comm_utils.def("remove_all",       &FileCommUtils::remove_all);
-    m_file_comm_utils.def("rename",           &FileCommUtils::rename);
-    m_file_comm_utils.def("exists",           &FileCommUtils::exists);
+    m_file_comm_utils.def("create_directory", &Helpers::create_directory);
+    m_file_comm_utils.def("rename",           &Helpers::rename);
+    m_file_comm_utils.def("remove",           &Helpers::remove);
+    m_file_comm_utils.def("remove_all",       &Helpers::remove_all);
+    m_file_comm_utils.def("rename",           &Helpers::rename);
+    m_file_comm_utils.def("exists",           &Helpers::exists);
 }
--- tests/test_file_comm.py
+++ tests/test_file_comm.py
@@ -25,7 +25,7 @@ class TestFileCommUtilities(unittest.TestCase):
         self.assertFalse(os.path.isdir(TestFileCommUtilities.dir_name))
 
         # call function to be checked
-        communication_tests.FileCommUtils.create_directory(TestFileCommUtilities.dir_name)
+        communication_tests.FileSystem.create_directory(TestFileCommUtilities.dir_name)
 
         # preform checks
         self.assertTrue(os.path.isdir(TestFileCommUtilities.dir_name))
@@ -38,7 +38,7 @@ class TestFileCommUtilities(unittest.TestCase):
         self.assertTrue(os.path.isfile(TestFileCommUtilities.file_name))
 
         # call function to be checked
-        communication_tests.FileCommUtils.remove(TestFileCommUtilities.file_name)
+        communication_tests.FileSystem.remove(TestFileCommUtilities.file_name)
 
         # preform checks
         self.assertFalse(os.path.isdir(TestFileCommUtilities.file_name))
@@ -50,7 +50,7 @@ class TestFileCommUtilities(unittest.TestCase):
         self.assertTrue(os.path.isdir(TestFileCommUtilities.dir_name))
 
         # call function to be checked
-        communication_tests.FileCommUtils.remove(TestFileCommUtilities.dir_name)
+        communication_tests.FileSystem.remove(TestFileCommUtilities.dir_name)
 
         # preform checks
         self.assertFalse(os.path.isdir(TestFileCommUtilities.dir_name))
@@ -67,52 +67,52 @@ class TestFileCommUtilities(unittest.TestCase):
 
         # call function to be checked
         with self.assertRaisesRegex(RuntimeError, "not empty"):
-            communication_tests.FileCommUtils.remove(TestFileCommUtilities.dir_name)
+            communication_tests.FileSystem.remove(TestFileCommUtilities.dir_name)
 
-        communication_tests.FileCommUtils.remove_all(TestFileCommUtilities.dir_name)
+        communication_tests.FileSystem.remove_all(TestFileCommUtilities.dir_name)
 
         # can be called multiple times
-        communication_tests.FileCommUtils.remove_all(TestFileCommUtilities.dir_name)
+        communication_tests.FileSystem.remove_all(TestFileCommUtilities.dir_name)
 
         # preform checks
         self.assertFalse(os.path.isdir(TestFileCommUtilities.dir_name))
 
     def test_exists_file(self):
         # call function to be checked
-        self.assertFalse(communication_tests.FileCommUtils.exists(TestFileCommUtilities.file_name))
+        self.assertFalse(communication_tests.FileSystem.exists(TestFileCommUtilities.file_name))
 
         # prepare test
         with open(TestFileCommUtilities.file_name, 'w') as test_file:
             test_file.write("hello")
 
         # call function to be checked
-        self.assertTrue(communication_tests.FileCommUtils.exists(TestFileCommUtilities.file_name))
+        self.assertTrue(communication_tests.FileSystem.exists(TestFileCommUtilities.file_name))
 
     def test_exists_directory(self):
         # call function to be checked
-        self.assertFalse(communication_tests.FileCommUtils.exists(TestFileCommUtilities.dir_name))
+        self.assertFalse(communication_tests.FileSystem.exists(TestFileCommUtilities.dir_name))
 
         # prepare test
         os.mkdir(TestFileCommUtilities.dir_name)
 
         # call function to be checked
-        self.assertTrue(communication_tests.FileCommUtils.exists(TestFileCommUtilities.dir_name))
+        self.assertTrue(communication_tests.FileSystem.exists(TestFileCommUtilities.dir_name))
 
     def test_exists_file_in_directory(self):
         # call function to be checked
-        self.assertFalse(communication_tests.FileCommUtils.exists(TestFileCommUtilities.file_in_dir_name))
+        self.assertFalse(communication_tests.FileSystem.exists(TestFileCommUtilities.file_in_dir_name))
 
         # prepare test
         os.mkdir(TestFileCommUtilities.dir_name)
 
         # call function to be checked
-        self.assertFalse(communication_tests.FileCommUtils.exists(TestFileCommUtilities.file_in_dir_name))
+        self.assertFalse(communication_tests.FileSystem.exists(TestFileCommUtilities.file_in_dir_name))
 
         with open(TestFileCommUtilities.file_in_dir_name, 'w') as test_file:
             test_file.write("hello")
 
         # call function to be checked
-        self.assertTrue(communication_tests.FileCommUtils.exists(TestFileCommUtilities.file_in_dir_name))
+        self.assertTrue(communication_tests.FileSystem.exists(TestFileCommUtilities.file_in_dir_name))
 
         # cleanup
         shutil.rmtree(TestFileCommUtilities.dir_name)
@@ -127,7 +127,7 @@ class TestFileCommUtilities(unittest.TestCase):
         new_file_name = "aassdd" + TestFileCommUtilities.file_name
 
         # call function to be checked
-        communication_tests.FileCommUtils.rename(TestFileCommUtilities.file_name, new_file_name)
+        communication_tests.FileSystem.rename(TestFileCommUtilities.file_name, new_file_name)
 
         # preform checks
         self.assertFalse(os.path.isfile(TestFileCommUtilities.file_name))
