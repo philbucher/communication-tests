d191e569a7e54efb63a63b4fe20f5fe86f9b3549
Philipp <philipp.bucher@tum.de>
2019-12-10 18:08:03 +0100

    minor cleanup

--- common.h
+++ common.h
@@ -2,27 +2,10 @@
 
 #include <iostream>
 #include <stdexcept>
-struct err { // helper struct to mimic behavior of KRATOS_ERROR
+struct err {
 err() {std::cout << "Error: ";}
 ~err() noexcept(false) { throw std::exception(); } // destructors are noexcept by default
 };
 #define COMM_TESTS_ERROR (err(), std::cout)
-
-// #if __cplusplus >= 201703L && (__has_include(<filesystem>) || __has_include(<experimental/filesystem>))
-//     #define FILESYSTEM_AVAILABLE
-// #endif
-
-// #if defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
-//     #define KRATOS_COMPILED_IN_LINUX
-
-// #elif defined(__APPLE__) && defined(__MACH__)
-//     #define KRATOS_COMPILED_IN_OSX
-
-// #elif defined(_WIN32)
-//     #define KRATOS_COMPILED_IN_WINDOWS
-// #endif
-
-// add macros to check for compiler support
-// add macros to check in which OS the compilation is done
 
 // TODO check configuration => I think Win by default builds debug! => works but has to be changed in case I do performace evaluations!
