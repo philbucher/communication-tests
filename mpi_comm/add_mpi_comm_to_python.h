2d54f1fa44725927d8b8bb6a1059f0908cf7b91f
philbucher <philipp.bucher@tum.de>
2019-12-10 10:06:31 +0100

    missing includes

--- mpi_comm/add_mpi_comm_to_python.h
+++ mpi_comm/add_mpi_comm_to_python.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <pybind11/pybind11.h>
+#include "mpi_comm.h"
 
 void AddMPICommToPython(pybind11::module& m)
 {
--- sockets_comm/add_sockets_comm_to_python.h
+++ sockets_comm/add_sockets_comm_to_python.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <pybind11/pybind11.h>
+#include "sockets_comm.h"
 
 void AddSocketsCommToPython(pybind11::module& m)
 {
